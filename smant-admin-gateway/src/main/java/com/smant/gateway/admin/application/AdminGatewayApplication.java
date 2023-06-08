58bb1c51491f6c182f4b450ddac5d59aad8e4c52
kingow <kingow@163.com>
2023-06-08 09:34:11 +0800

    搭建项目:admin gateway,api gateway,external gateway

--- smant-admin-gateway/src/main/java/com/smant/gateway/admin/application/AdminGatewayApplication.java
+++ smant-admin-gateway/src/main/java/com/smant/gateway/admin/application/AdminGatewayApplication.java
@@ -1,4 +1,20 @@
 package com.smant.gateway.admin.application;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.SpringVersion;
+
+@SpringBootApplication(
+        scanBasePackages = {"com.smant.gateway.admin"},exclude = {DataSourceAutoConfiguration.class })
+@Slf4j
 public class AdminGatewayApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(AdminGatewayApplication.class)
+                .main(SpringVersion.class)
+                .bannerMode(Banner.Mode.CONSOLE)
+                .run(args);
+    }
 }
