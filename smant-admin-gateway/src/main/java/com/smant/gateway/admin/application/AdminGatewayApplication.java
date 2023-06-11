af4305f372d22744c8432eb1919b5f05c74f954f
kingow <kingow@163.com>
2023-06-12 06:01:36 +0800

    j;

--- smant-admin-gateway/src/main/java/com/smant/gateway/admin/application/AdminGatewayApplication.java
+++ smant-admin-gateway/src/main/java/com/smant/gateway/admin/application/AdminGatewayApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.SpringVersion;
 
 @SpringBootApplication(
         scanBasePackages = {"com.smant.gateway.admin"},exclude = {DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 @Slf4j
 public class AdminGatewayApplication {
     public static void main(String[] args) {
